c0af7a6650cea50004666eaf0daa0e722c1a1e05
voideh <jakedlopez@gmail.com>
2016-08-01 11:11:00 -0400

    now with some css animations

---
+++ js/popUp.js
@@ -0,0 +1,43 @@
+/* PopUp! 
+	by jake Lopez
+
+just popup those images and make em look okay...
+thats the main goal right now.
+	 */
+var doc = $("body");
+var img = $(".popup");
+
+var underDiv = '<div id="base"></div>'
+var div = '<div class="popup"></div>'
+
+
+$("a#popIt").bind('click', function(event)
+{
+	event.preventDefault();
+	popUp(this);
+});
+
+function popUp(elem)
+{
+	doc.append(underDiv);
+	$("#base").append(div);
+	var img = '<figure id="pop"><img src="' + elem.href + '" id="pop"></figure>';
+	setTimeout(function(){ 
+	$("div.popup").append(img);
+	$("div.popup").append('<button id="quit">X</button>');
+	$("<img/>").attr("src", $("img#pop").attr("src"))
+		.on("load",function(){
+		if(this.height > 900 && this.width > 900)
+		{
+		// resize the image...???
+			$("img#pop").css("height", "85%");
+			$("img#pop").after('<figcaption class="caption">This image has been rescaled.</figcaption>');
+		}
+		$("div.popup").css("height", this.height + 200);
+		$("div.popup").css("width", this.width + 200);
+		});	
+		$("button#quit").click(function()
+		{$("#base").remove(); });
+}, 500);
+}
+
